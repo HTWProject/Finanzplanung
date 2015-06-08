f440a49273df3ab5edac7125908a62b199f34f43
Fomenko <s0539589@htw-berlin.de>
2015-06-08 11:17:39 +0200

    update von Startbildschirm

--- app/src/main/java/com/htw/finanzplanung/ActivityLogin.java
+++ app/src/main/java/com/htw/finanzplanung/ActivityLogin.java
@@ -12,6 +12,8 @@ public class ActivityLogin extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
     }
 
     @Override
