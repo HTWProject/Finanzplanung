d82abb86c127b7aaf16c5edb9e263230a7389053
Fomenko <s0539589@htw-berlin.de>
2015-06-21 16:18:50 +0200

    Activity Home, Settings und Registration wurden erstrellt und Login Activity wurde verbessert

--- app/src/main/java/com/htw/finanzplanung/ActivityHome.java
+++ app/src/main/java/com/htw/finanzplanung/ActivityHome.java
@@ -1,74 +1,59 @@
 package com.htw.finanzplanung;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 
-public class ActivityLogin extends AppCompatActivity{
-
-    // Anlegen der Variabeln
-    EditText inputEmail;
-    EditText inputPasswort;
+public class ActivityHome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_home);
 
-        // Zuweisung der XML Objekte an unsere Variabeln
-        inputEmail = (EditText) findViewById(R.id.email);
-        inputPasswort = (EditText) findViewById(R.id.passwort);
-        Button registrationButton = (Button) findViewById(R.id.bt_registration);
-        Button loginButton = (Button) findViewById(R.id.bt_login);
+
+        Button settingsButton = (Button) findViewById(R.id.bt_settings);
+        Button gruppenverwaltungButton = (Button) findViewById(R.id.bt_gruppen);
+
 
         // ClickListener implementieren für den Button zum Wechsel der Activity
-        registrationButton.setOnClickListener(new View.OnClickListener() {
+        settingsButton.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View arg0) {
                 //Neues Intent anlegen
-                Intent nextScreen = new Intent(getApplicationContext(), ActivityRegistration.class);
-
-                //Intent mit den Daten füllen
-                nextScreen.putExtra("Email", inputEmail.getText().toString());
-                nextScreen.putExtra("Passwort", inputPasswort.getText().toString());
-
-
+                Intent nextScreen = new Intent(getApplicationContext(), ActivitySettings.class);
                 // Intent starten und zur zweiten Activity wechseln
                 startActivity(nextScreen);
 
             }
         });
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+
+        // ClickListener implementieren für den Button zum Wechsel der Activity
+        gruppenverwaltungButton.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View arg0) {
                 //Neues Intent anlegen
-                Intent nextScreen = new Intent(getApplicationContext(), ActivityHome.class);
-
-                //Intent mit den Daten füllen
-                nextScreen.putExtra("Email", inputEmail.getText().toString());
-                nextScreen.putExtra("Passwort", inputPasswort.getText().toString());
-
-
+                Intent nextScreen = new Intent(getApplicationContext(), ActivitySettings.class);
                 // Intent starten und zur zweiten Activity wechseln
                 startActivity(nextScreen);
 
             }
         });
+
+
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
+        getMenuInflater().inflate(R.menu.menu_activity_home, menu);
         return true;
     }
 
---
+++ app/src/main/java/com/htw/finanzplanung/ActivityRegistration.java
@@ -0,0 +1,62 @@
+package com.htw.finanzplanung;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class ActivityRegistration extends AppCompatActivity {
+
+    // Anlegen der Variabeln
+    EditText inputName;
+    EditText inputEmail;
+    EditText inputPasswort;
+    EditText inputPasswortValidierung;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        // Zuweisung der XML Objekte an unsere Variabeln
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPasswort = (EditText) findViewById(R.id.passwort);
+        inputPasswortValidierung = (EditText) findViewById(R.id.PasswortValidation);
+
+        Button okButton = (Button) findViewById(R.id.bt_OK);
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_activity_registration, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
