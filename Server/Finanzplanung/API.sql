d35a613f5ef1c6d39970e537b3f86376fa50f991
Fomenko <s0539589@htw-berlin.de>
2015-06-29 10:32:32 +0200

    Server PHP Script erstellt

---
+++ Server/Finanzplanung/API.sql
@@ -0,0 +1,92 @@
+
+DROP PROCEDURE IF EXISTS s0539589.registration;
+delimiter //
+CREATE PROCEDURE s0539589.registration(IN $email VARCHAR(255), IN $name VARCHAR(255), IN $passwort VARCHAR(255), IN $ip VARCHAR(255), IN $aktiviert VARCHAR(255))
+BEGIN
+	IF ($email NOT REGEXP '^[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$' OR $email NOT LIKE '%_@_%._%') THEN
+		SELECT ("Registration was not sucessfulf, write your email correct") AS exception;
+	ELSEIF ($passwort = ' ' AND $passwort = '') THEN
+		SELECT ("Registration was not sucessfulf, write your password") AS exception;
+	ELSEIF ($name = ' ' AND $name = '' ) THEN
+		SELECT ("Registration was not sucessfulf, write your name") AS exception;
+	ELSEIF EXISTS (SELECT * FROM user WHERE email = TRIM($email)) then
+		SELECT ("Registration was not sucessfulf, try other email because there are a user with this email") AS exception;
+	ELSE 
+		INSERT INTO user(`email`,`name`,`passwort`,`ip`, `aktiviert`) values(TRIM($email),$name,TRIM($passwort),$ip,$aktiviert);
+		SELECT ("Registration was sucessfull, Check your eMail Adress fot the Activationslink") AS exception, last_insert_id() AS _id;
+	END IF;
+END//
+delimiter ;
+
+
+DROP PROCEDURE IF EXISTS s0539589.aktivierung;
+delimiter //
+CREATE PROCEDURE s0539589.aktivierung(IN $account_id INT UNSIGNED, IN $email VARCHAR(255), IN $aktiviert VARCHAR(255))
+BEGIN
+	IF EXISTS(SELECT * FROM user WHERE _id=$account_id AND aktiviert = $aktiviert AND email = $email) THEN
+		UPDATE user SET aktiviert='1' WHERE _id=$account_id;
+	END IF;
+END//
+delimiter ;
+
+
+DROP PROCEDURE IF EXISTS s0539589.Login;
+delimiter //
+CREATE PROCEDURE s0539589.Login(IN $email VARCHAR(255),IN $passwort VARCHAR(255),IN $ip VARCHAR(255))
+BEGIN
+	UPDATE user SET `online` = CURRENT_TIMESTAMP, `ip` = $ip  WHERE email = $email AND passwort = $passwort;
+	SELECT _id, name, email, aktiviert FROM user WHERE email = $email AND passwort = $passwort;
+END//
+delimiter ;
+
+
+DROP PROCEDURE IF EXISTS s0539589.LoginFail;
+delimiter //
+CREATE PROCEDURE s0539589.LoginFail(IN $email VARCHAR(255))
+BEGIN
+	SELECT name, email, passwort FROM user WHERE email = $email AND aktiviert = '1';
+END//
+delimiter ;
+
+DROP PROCEDURE IF EXISTS s0539589.getLogin;
+delimiter //
+CREATE PROCEDURE s0539589.getLogin(IN $account_id INT UNSIGNED, IN $ip VARCHAR(255))
+BEGIN
+	UPDATE user SET `online` = CURRENT_TIMESTAMP, `ip` = $ip  WHERE _id = $account_id;
+	SELECT _id, name, email, aktiviert FROM user WHERE _id = $account_id;
+END//
+delimiter ;
+
+
+DROP PROCEDURE IF EXISTS s0539589.Login;
+delimiter //
+CREATE PROCEDURE s0539589.Login(IN $email VARCHAR(255),IN $passwort VARCHAR(255),IN $ip VARCHAR(255))
+BEGIN
+	UPDATE user SET `online` = CURRENT_TIMESTAMP, `ip` = $ip  WHERE email = $email AND passwort = $passwort;
+	SELECT _id, name, email, aktiviert FROM user WHERE email = $email AND passwort = $passwort;
+END//
+delimiter ;
+
+
+DROP PROCEDURE IF EXISTS s0539589.getPasswort;
+delimiter //
+CREATE PROCEDURE s0539589.getPasswort(IN $email VARCHAR(255))
+BEGIN
+	SELECT passwort FROM user WHERE email = $email;
+END//
+delimiter ;
+
+
+
+
+
+DROP PROCEDURE IF EXISTS s0539589.setPasswort;
+delimiter //
+CREATE PROCEDURE s0539589.setPasswort(IN $account_id INT UNSIGNED, IN $oldpasswort VARCHAR(255), IN $newpasswort VARCHAR(255), IN $ip VARCHAR(255))
+BEGIN
+	UPDATE user SET `online`= CURRENT_TIMESTAMP, `ip` = $ip, `passwort` = $newpasswort WHERE `_id` = $account_id AND `passwort` = $oldpasswort;
+END//
+delimiter ;
+
+
+
---
+++ Server/Finanzplanung/db_connect.php
@@ -0,0 +1,14 @@
+<?php
+$db_conx = mysqli_connect("cgi.rz.htw-berlin.de","s0539589","Passwort","s0539589");		
+
+	
+
+if (mysqli_connect_errno()) {
+    echo mysqli_connect_error();
+    exit();
+} else {
+	if (!mysqli_query($db_conx, "SET CHARACTER SET utf8")) {
+    		printf("Error: %s\n", mysqli_sqlstate($db_conx));
+	}
+}
+?>
